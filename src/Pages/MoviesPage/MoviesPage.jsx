986e91ab957704d105fd90bce265791ad53bd4a5
Romi Landau <romi7102@gmail.com>
2023-08-28 17:44:45 +0300

    movies and series pages

--- src/Pages/MoviesPage/MoviesPage.jsx
+++ src/Pages/MoviesPage/MoviesPage.jsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React,{useEffect, useContext} from "react";
 import Navbar from "../../Components/Navbar/Navbar";
 import Billboard from "../../Components/Billboard/Billboard";
+import useFeaturedContent from "../../Hooks/useFeaturedContent";
+import FeaturedContentCarousel from "../../Components/FeaturedContentCarousel/FeaturedContentCarousel";
+import { Store } from "../../Context/StoreProvider";
 
 const MoviesPage = () => {
+  const { data, error, isLoading } = useFeaturedContent("movie");
+
+  const { state } = useContext(Store);
+  const { userInfo } = state;
+
+  useEffect(() => {
+    if (!userInfo) {
+      navigate("/login");
+    }
+  }, [state, userInfo]);
+
   return (
     <>
       <Navbar></Navbar>
       <Billboard type="movie" />
+
+      <div>
+        {isLoading ? (
+          <h1 className="text-white">Loading...</h1>
+        ) : error ? (
+          <h1 className="text-white">Error...</h1>
+        ) : data && 
+          (data.map((f) => (<FeaturedContentCarousel key={f._id} data={f}></FeaturedContentCarousel>)))
+        }
+        
+      </div>
     </>
   );
 };
--- src/Pages/SeriesPage/SeriesPage.jsx
+++ src/Pages/SeriesPage/SeriesPage.jsx
@@ -1,14 +1,39 @@
-import React from 'react'
-import Billboard from '../../Components/Billboard/Billboard'
-import Navbar from '../../Components/Navbar/Navbar'
+import React,{useEffect, useContext} from "react";
+import Navbar from "../../Components/Navbar/Navbar";
+import Billboard from "../../Components/Billboard/Billboard";
+import useFeaturedContent from "../../Hooks/useFeaturedContent";
+import FeaturedContentCarousel from "../../Components/FeaturedContentCarousel/FeaturedContentCarousel";
+import { Store } from "../../Context/StoreProvider";
 
-const SeriesPage = () => {
+const MoviesPage = () => {
+  const { data, error, isLoading } = useFeaturedContent("series");
+
+  const { state } = useContext(Store);
+  const { userInfo } = state;
+
+  useEffect(() => {
+    if (!userInfo) {
+      navigate("/login");
+    }
+  }, [state, userInfo]);
+
   return (
     <>
-        <Navbar></Navbar>
-        <Billboard type="series"></Billboard>
-    </>
-  )
-}
+      <Navbar></Navbar>
+      <Billboard type="series" />
 
-export default SeriesPage
+      <div>
+        {isLoading ? (
+          <h1 className="text-white">Loading...</h1>
+        ) : error ? (
+          <h1 className="text-white">Error...</h1>
+        ) : data && 
+          (data.map((f) => (<FeaturedContentCarousel key={f._id} data={f}></FeaturedContentCarousel>)))
+        }
+        
+      </div>
+    </>
+  );
+};
+
+export default MoviesPage;
