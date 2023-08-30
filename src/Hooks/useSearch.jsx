b9f083df401ee684b0c8e66456b7f2f2d7230f9c
Romi Landau <romi7102@gmail.com>
2023-08-30 13:19:28 +0300

    implemented working search page

---
+++ src/Hooks/useSearch.jsx
@@ -0,0 +1,18 @@
+import useSWR from "swr";
+import fetcher from "../libs/Fetcher";
+
+const useSearch = (query) => {
+  const { data, error, isLoading } = useSWR(`/content/search?query=${query}`, fetcher, {
+    revalidateIfStale: false,
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+  });
+
+  return {
+    data,
+    error,
+    isLoading,
+  };
+};
+
+export default useSearch;
--- src/Pages/SearchPage/SearchPage.jsx
+++ src/Pages/SearchPage/SearchPage.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import Navbar from '../../Components/Navbar/Navbar'
+import React, { useEffect, useState } from "react";
+import Navbar from "../../Components/Navbar/Navbar";
+import useSearch from "../../Hooks/useSearch";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
+import ContentCard from "../../Components/ContentCard/ContentCard";
+import NetflixSpinner from "../../Components/NetflixSpinner/NetflixSpinner";
 
 const SearchPage = () => {
-  return (
-    <></>
-  )
-}
+  const [query, setQuery] = useState("");
 
-export default SearchPage
+  const { search } = useLocation();
+  console.log(search);
+  const searchParams = new URLSearchParams(search);
+  console.log(query);
+  const { data, error, isLoading } = useSearch(query);
+
+  useEffect(() => {
+    setQuery(searchParams.get("q"));
+  }, [search, searchParams]);
+
+  return (
+    <>
+      <div className=" h-36"></div>
+      {isLoading != true && <h1 className="text-white text-2xl mx-10 lg:mx-15">Showing search results for "{query}":</h1>}
+      <div className="grid grid-cols-5 gap-2 mt-4 mx-10 lg:mx-16 relative">
+        {isLoading ? (
+          <NetflixSpinner/>
+        ) : error ? (
+          <h1>Error!!!</h1>
+        ) : data && (
+          data.contents.map((c) => <div><ContentCard key={c._id} content={c}></ContentCard></div>)
+        )}
+      </div>
+    </>
+  );
+};
+
+export default SearchPage;
