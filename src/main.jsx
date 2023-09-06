28bcbb646ab790f0801db352730d72d078bf1f9e
Romi Landau <romi7102@gmail.com>
2023-09-06 10:38:31 +0300

    added env vars

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,9 @@ import axios from "axios";
 import { StoreProvider } from "./Context/StoreProvider.jsx";
 
 
-axios.defaults.baseURL = "http://localhost:5000/api";
+axios.defaults.baseURL = import.meta.env.VITE_API_ROUTE ? `${import.meta.env.VITE_API_ROUTE}/api` : "http://localhost:5000/api";
 axios.defaults.headers.common['Authorization'] = "";
+console.log(import.meta.env.VITE_API_ROUTE);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
